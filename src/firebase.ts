be97d7f4846d7d13d837f6c0c9dbc95802287265
thienanga075 <pdtduong@gmail.com>
2022-07-31 16:18:28 +0700

    deploy

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrYHGR0TYbTUsckrPnS8UeJtiMANwlvhg",
+  authDomain: "finance-app-c0c7b.firebaseapp.com",
+  projectId: "finance-app-c0c7b",
+  storageBucket: "finance-app-c0c7b.appspot.com",
+  messagingSenderId: "716864219599",
+  appId: "1:716864219599:web:dc6a54cb958f2cdd5b0aba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
